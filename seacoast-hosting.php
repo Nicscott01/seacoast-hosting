d4e627639d4f5098c8e9a2e4599ff7f00365138d
Nic Scott <nic@crearewebsolutions.com>
2020-10-22 18:01:45 +0000

    Initial commit

---
+++ seacoast-hosting.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *      Plugin Name: Seacoast Hosting
+ *      Author: Creare Web Solutions
+ *      Version: 0.1
+ *      Description: Helper plugin to handle user accounts and password storage
+ *
+ */
+
